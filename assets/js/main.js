967cc83a830fc6323340bfa02ca4b8dd30a924ae
Ginx96 <ginna.carpio@gmail.com>
2024-12-01 20:17:43 -0500

    cambios logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
 /**
-* Template Name: Restaurantly
-* Updated: Sep 20 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/restaurantly-restaurant-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
+\
 */
 (function() {
   "use strict";
